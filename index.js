bdfc4d02eb46916789c5e4a2c70d76ced69eee23
Kirtika Jain <jain.kirtika08@gmail.com>
2023-09-19 20:00:32 +0530

    feat: initialize sequlize

--- index.js
+++ index.js
@@ -1,45 +1,37 @@
 require("dotenv").config();
 const express = require("express");
+const bodyParser = require("body-parser");
 const { Pool } = require("pg"); ;
 const app = express();
+const db = require("./models/index.js");
 const studentsRoutes = require("./routes/student.routes.js");
 const coursesRoutes = require("./routes/course.routes.js");
 
 
 let PORT = 8082;
 
+
+app.use(express.json());
 app.use("/students",studentsRoutes);
 app.use("/courses",coursesRoutes);
 
-const pool = new Pool({
-      user: process.env.PGUSER,
-      host: process.env.PGHOST,
-       database: process.env.PGDATABASE,
-       password: process.env.PGPASSWORD,
-       port: process.env.PGPORT,
-});
-pool.connect();
 
-//const connectDb = async () => {
-//    try {
-//        const pool = new Pool({
-//            user: process.env.PGUSER,
-//            host: process.env.PGHOST,
-//            database: process.env.PGDATABASE,
-//            password: process.env.PGPASSWORD,
-//            port: process.env.PGPORT,
-//        });
-//
-//        await pool.connect()
-//        const res = await pool.query('SELECT * FROM clients')
-//        console.log(res)
-//        await pool.end()
-//    } catch (error) {
-//        console.log(error)
-//    }
-//}
+db.sequelize.sync()
+  .then(() => {
+    console.log("Synced db.");
+  })
+  .catch((err) => {
+    console.log("Failed to sync db: " + err.message);
+  });
 
-//connectDb()
+//const pool = new Pool({
+//      user: process.env.PGUSER,
+//      host: process.env.PGHOST,
+//       database: process.env.PGDATABASE,
+//       password: process.env.PGPASSWORD,
+//       port: process.env.PGPORT,
+//});
+//pool.connect();
 
 app.listen(PORT, () => {
   console.log("Server Listening at", PORT);
